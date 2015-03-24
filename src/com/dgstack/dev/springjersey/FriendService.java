da24e625e61ed3e816937ac66f4f954facd56a08
digvijayb <digvijay.bhakuni@rediffmail.com>
2015-03-25 02:14:57 +0530

    init commit

---
+++ src/com/dgstack/dev/springjersey/FriendService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.dgstack.dev.springjersey;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author digvijayb
+ *
+ */@Path("/")
+ @Component
+public class FriendService {
+
+	/**
+	 * 
+	 */
+	public FriendService() {
+	}
+	
+	
+
+}
